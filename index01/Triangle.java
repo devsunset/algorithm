ea49f635406fb79f2cd62701ed1da7ae100b9633
devsunset <devsunset@gmail.com>
2022-02-15 12:27:12 +0900

    dev

--- index01/Triangle.java
+++ index01/Triangle.java
@@ -59,9 +59,23 @@ public class Triangle {
     }
 
     public static void spira(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++)
+                System.out.print(' ');
+            for (int j = 1; j <= (i - 1) * 2 + 1; j++)
+                System.out.print('*');
+            System.out.println();
+        }
         System.out.println("----------------------");
     }
     public static void npira(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++)
+                System.out.print(' ');
+            for (int j = 1; j <= (i - 1) * 2 + 1; j++)
+                System.out.print(i % 10);
+            System.out.println();
+        }
         System.out.println("----------------------");
     }
 }
