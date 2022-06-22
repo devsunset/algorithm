7e2b5cbb0b1b4cd4091ab0d2bed34ecdedc27647
devsunset <devsunset@gmail.com>
2022-06-22 09:43:25 +0900

    dev

---
+++ index02/ArrayStructure.java
@@ -0,0 +1,17 @@
+package index02;
+
+public class ArrayStructure {
+
+    public static void main(String[] args){
+       int[] a = new int[5];
+       a[0] = 00;
+       a[1] = 11;
+       a[2] = 12;
+       a[3] = 13;
+       a[4] = 14;
+
+       for (int i = 0; i < a.length; i++){
+           System.out.println(a[i]);
+       }
+    }
+}
